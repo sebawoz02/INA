30f683777496b6d39ee6c0ed3aaa74f9a4eb378e
sebawoz02 <sebawoz2002@gmail.com>
2023-10-11 15:41:06 +0200

    del

--- AM/l1/app_z2/main.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by sebastian on 10/11/23.
-//
-int main(int argc, char* argv[])
-{
-    (void) argv;
-    (void) argc;
-    return 0;
-}
--- AM/l1/src/mst.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by sebastian on 10/11/23.
-//
-#include <mst.h>
-
-void calc_mst(void){
-    return;
-}
